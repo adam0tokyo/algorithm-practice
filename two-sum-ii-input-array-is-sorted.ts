4450a944bb984669fb22f4d05673885c4036086a
adam0tokyo <adam0tokyo@gmail.com>
2023-10-25 01:08:54 +0900

    update two-sum-ii-input-array-is-sorted.js

--- two-sum-ii-input-array-is-sorted.ts
+++ two-sum-ii-input-array-is-sorted.ts
@@ -1,20 +1,35 @@
 
 //66 ms ~33%
 //43.7 MB ~47%
-function twoSumTwo(nums: number[], target: number): number[] {
-    const hashMap: {[key:string]: number} = {};
-    let complement: number;
-    let output = [0,0];
+// function twoSumTwo(nums: number[], target: number): number[] {
+//     const hashMap: {[key:string]: number} = {};
+//     let complement: number;
+//     let output = [0,0];
 
-    for (let i = 0; i < nums.length; i++) {
-        complement = target - nums[i];
-        if (hashMap.hasOwnProperty(complement)) {
-            output = [hashMap[complement] + 1, i + 1]
-            break;
-        }
-        hashMap[nums[i]] = i;
+//     for (let i = 0; i < nums.length; i++) {
+//         complement = target - nums[i];
+//         if (hashMap.hasOwnProperty(complement)) {
+//             output = [hashMap[complement] + 1, i + 1]
+//             break;
+//         }
+//         hashMap[nums[i]] = i;
+//     }
+//     return output;
+// };
+
+
+//63 ms ~44%
+//43.7 MB ~47%
+// This is a much more elegant solution, but it is od that performance isn't much different
+function twoSumTwo(nums: number[], target: number): number[] {
+    let left = 0;
+    let right = nums.length -1;
+    while(left < right) {
+        if ((nums[left] + nums[right]) > target) right --;
+        else if ((nums[left] + nums[right]) < target) left ++;
+        else break;
     }
-    return output;
+    return [left + 1, right + 1];
 };
 
 console.log(twoSumTwo([2,7,11,15], 9)) //[1,2]
